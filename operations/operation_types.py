3f2a7bd5846b6715d27dc9a62a5e111646e78c3d
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 14:47:14 -0400

    Operation types

---
+++ operations/operation_types.py
@@ -0,0 +1,62 @@
+# Arithmetic Operations
+num1 = 10
+num2 = 3
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponentiation = num1 ** num2
+
+print("Arithmetic Operations:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
+
+# Comparison Operations
+x = 5
+y = 7
+
+equal = x == y
+not_equal = x != y
+greater_than = x > y
+less_than = x < y
+greater_than_equal = x >= y
+less_than_equal = x <= y
+
+print("\nComparison Operations:")
+print("Equal:", equal)
+print("Not Equal:", not_equal)
+print("Greater Than:", greater_than)
+print("Less Than:", less_than)
+print("Greater Than or Equal:", greater_than_equal)
+print("Less Than or Equal:", less_than_equal)
+
+# Logical Operations
+p = True
+q = False
+
+logical_and = p and q
+logical_or = p or q
+logical_not = not p
+
+print("\nLogical Operations:")
+print("Logical AND:", logical_and)
+print("Logical OR:", logical_or)
+print("Logical NOT:", logical_not)
+
+# Membership Operations
+fruits = ["apple", "banana", "cherry"]
+
+is_apple_in_fruits = "apple" in fruits
+is_mango_not_in_fruits = "mango" not in fruits
+
+print("\nMembership Operations:")
+print("Is 'apple' in fruits?", is_apple_in_fruits)
+print("Is 'mango' not in fruits?", is_mango_not_in_fruits)

d20d0ef77afb357877f4900d8fdc0f185b2795b5
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 15:02:17 -0400

    Control structures

---
+++ control_structures/control_structures.py
@@ -0,0 +1,31 @@
+# If statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# If-else statement
+y = 3
+if y > 5:
+    print("y is greater than 5")
+else:
+    print("y is not greater than 5")
+
+# If-elif-else statement
+z = 7
+if z > 10:
+    print("z is greater than 10")
+elif z > 5:
+    print("z is greater than 5 but not greater than 10")
+else:
+    print("z is not greater than 5")
+
+# For loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# While loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1

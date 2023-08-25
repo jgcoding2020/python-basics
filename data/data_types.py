a1a325fbc0da58e33a9e869390e0de833752768b
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 14:43:50 -0400

    Data types

---
+++ data/data_types.py
@@ -0,0 +1,51 @@
+# Integer
+my_integer = 42
+print("Integer:", my_integer)
+
+# Float
+my_float = 3.14
+print("Float:", my_float)
+
+# String
+my_string = "Hello, World!"
+print("String:", my_string)
+
+# Boolean
+my_boolean = True
+print("Boolean:", my_boolean)
+
+# List (ordered collection)
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Tuple (immutable ordered collection)
+my_tuple = (10, 20, 30)
+print("Tuple:", my_tuple)
+
+# Dictionary (key-value pairs)
+my_dict = {"name": "Alice", "age": 30}
+print("Dictionary:", my_dict)
+
+# Set (unordered collection of unique elements)
+my_set = {1, 2, 3, 3, 4, 4, 5}
+print("Set:", my_set)
+
+# Complex Number
+my_complex = 2 + 3j
+print("Complex Number:", my_complex)
+
+# None (represents the absence of a value)
+my_none = None
+print("None:", my_none)
+
+# Boolean operations
+result = True and False
+print("Boolean Operation (True and False):", result)
+
+# Type casting
+my_integer_from_float = int(my_float)
+print("Type Casting (Float to Integer):", my_integer_from_float)
+
+# String concatenation
+greeting = "Hello" + " " + "World!"
+print("String Concatenation:", greeting)

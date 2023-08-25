8c81b2438c1e19fad257af16dfb13a74ba5bef87
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 14:39:44 -0400

    Variables types

---
+++ variables/variables_types.py
@@ -0,0 +1,37 @@
+# Integer
+age = 30
+
+# Float
+pi = 3.14159
+
+# String
+name = "Alice"
+
+# Boolean
+is_student = True
+
+# List (ordered collection)
+fruits = ["apple", "banana", "cherry"]
+
+# Tuple (immutable ordered collection)
+point = (3, 5)
+
+# Dictionary (key-value pairs)
+person = {"name": "Bob", "age": 25}
+
+# Complex Number
+complex_num = 3 + 2j
+
+# None (represents the absence of a value)
+empty_variable = None
+
+# Displaying the values of variables
+print("Age:", age)
+print("Pi:", pi)
+print("Name:", name)
+print("Is a Student?", is_student)
+print("Fruits:", fruits)
+print("Point:", point)
+print("Person:", person)
+print("Complex Number:", complex_num)
+print("Empty Variable:", empty_variable)

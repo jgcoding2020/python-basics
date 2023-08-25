a3c6f11ac47de185f0c70bfea4c4c5f96dd51407
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 13:50:47 -0400

    Comment types

---
+++ comment_types.py
@@ -0,0 +1,22 @@
+# This is a single-line comment
+
+# Comments are used to add explanations or notes to code.
+# They are ignored by the Python interpreter.
+
+# Let's define a variable
+name = "Alice"  # This is an inline comment
+
+"""
+This is a
+multi-line
+comment.
+It can span
+multiple lines.
+"""
+
+# Triple quotes (single or double) can be used for multi-line comments.
+
+# You can also use comments to temporarily disable code:
+# print("This line won't run")
+
+# Adding comments helps improve code readability and maintainability.
